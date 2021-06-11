b7698272d7876818d3ae78fddae67edbad4e1f11
Wai Phyo <wai.phyo@jpl.nasa.gov>
2021-06-11 14:50:14 -0700

    fix: disable validating individual observation record coz it is too slow

--- parquet_flask/io_logic/sanitize_record.py
+++ parquet_flask/io_logic/sanitize_record.py
@@ -1,10 +1,39 @@
 import logging
 
+from parquet_flask.io_logic.cdms_constants import CDMSConstants
 from parquet_flask.utils.file_utils import FileUtils
 from parquet_flask.utils.general_utils import GeneralUtils
 
 LOGGER = logging.getLogger(__name__)
 
+
+basic_schema = {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "title": "Cloud-based Data Match-Up Service In Situ Schema",
+    "description": "Schema for in situ data",
+    "properties": {
+        "provider": {
+            "description": "",
+            "type": "string"
+        },
+        "project": {
+            "description": "",
+            "type": "string"
+        },
+        "observations": {
+            "type": "array",
+            "items": {
+                "type": "object"
+            },
+            "minItems": 1
+        }
+    },
+    "required": [
+        "provider",
+        "project",
+        "observations",
+    ]
+}
 
 class SanitizeRecord:
     def __init__(self, json_schema_path):
@@ -12,7 +41,7 @@ class SanitizeRecord:
         if not FileUtils.file_exist(json_schema_path):
             raise ValueError('json_schema file does not exist: {}'.format(json_schema_path))
         self.__json_schema = FileUtils.read_json(json_schema_path)
-        self.__schema_key_values = {k: v for k, v in self.__json_schema['definitions']['observation']['properties'].items()}
+        self.__schema_key_values = {k: v for k, v in self.__json_schema['properties'].items()}
 
     def __sanitize_record(self, data_blk):
         for k, v in data_blk.items():
@@ -26,9 +55,13 @@ class SanitizeRecord:
         if not FileUtils.file_exist(json_file_path):
             raise ValueError('json file does not exist: {}'.format(json_file_path))
         json_obj = FileUtils.read_json(json_file_path)
-        is_valid, json_error = GeneralUtils.is_json_valid(json_obj, self.__json_schema)
+        is_valid, json_error = GeneralUtils.is_json_valid(json_obj, basic_schema)
         if not is_valid:
             raise ValueError(f'input file has invalid schema: {json_file_path}. errors; {json_error}')
-        for each in json_obj:
+        for each in json_obj[CDMSConstants.observations_key]:
+            LOGGER.warning('disabling validation of individual observation record. it is taking a long time')
+            # is_valid, json_error = GeneralUtils.is_json_valid(each, self.__json_schema)
+            # if not is_valid:
+            #     raise ValueError(f'input file has invalid schema: {json_file_path}. errors; {json_error}')
             self.__sanitize_record(each)
         return json_obj
