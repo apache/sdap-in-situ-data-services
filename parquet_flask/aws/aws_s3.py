1df354bdc2f9699e39b073cda837dd0ecd66a0e5
Wai Phyo <wai.phyo@jpl.nasa.gov>
2021-06-11 06:22:05 -0700

    feat: add tag method + set url first before executing methods

--- parquet_flask/aws/aws_s3.py
+++ parquet_flask/aws/aws_s3.py
@@ -13,6 +13,12 @@ class AwsS3:
                                          aws_secret_access_key=Config().get_value('aws_secret_access_key'),
                                          aws_session_token=Config().get_value('aws_session_token'),
                                          region_name='us-west-2').client('s3')
+        self.__target_bucket = None
+        self.__target_key = None
+
+    def set_s3_url(self, s3_url):
+        self.__target_bucket, self.__target_key = self.split_s3_url(s3_url)
+        return self
 
     def split_s3_url(self, s3_url):
         s3_schema = [k for k in self.__valid_s3_schemas if s3_url.startswith(k)]
@@ -25,12 +31,46 @@ class AwsS3:
         key = s3_url[(split_index + s3_schema_length + 1):]
         return bucket, key
 
-    def download(self, s3_url, local_dir, file_name=None):
+    def __tag_existing_obj(self, other_tags={}):
+        if len(other_tags) == 0:
+            return
+        tags = {
+            'TagSet': []
+        }
+        for key, val in other_tags.items():
+            tags['TagSet'].append({
+                'Key': key,
+                'Value': str(val)
+            })
+        self.__s3_client.put_object_tagging(Bucket=self.__target_bucket, Key=self.__target_key, Tagging=tags)
+        return
+
+    def add_tags_to_obj(self, other_tags={}):
+        """
+        retrieve existing tags first and append new tags to them
+
+        :param bucket: string
+        :param s3_key: string
+        :param other_tags: dict
+        :return: bool
+        """
+        if len(other_tags) == 0:
+            return False
+        response = self.__s3_client.get_object_tagging(Bucket=self.__target_bucket, Key=self.__target_key)
+        if 'TagSet' not in response:
+            return False
+        all_tags = {k['Key']: k['Value'] for k in response['TagSet']}
+        for k, v in other_tags.items():
+            all_tags[k] = v
+            pass
+        self.__tag_existing_obj(all_tags)
+        return True
+
+    def download(self, local_dir, file_name=None):
         if not FileUtils.dir_exist(local_dir):
             raise ValueError('missing directory')
-        bucket, key = self.split_s3_url(s3_url)
         if file_name is None:
-            file_name = os.path.basename(s3_url)
+            file_name = os.path.basename(self.__target_key)
         local_file_path = os.path.join(local_dir, file_name)
-        self.__s3_client.download_file(bucket, key, local_file_path)
+        self.__s3_client.download_file(self.__target_bucket, self.__target_key, local_file_path)
         return local_file_path
