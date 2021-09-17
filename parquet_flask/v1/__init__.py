123afcb3982e5df31678826eb0a52f394b659283
Josh Garde <jgarde@jpl.nasa.gov>
2021-09-17 15:04:46 -0700

    Initial SwaggerUI deployment + OpenAPI spec

--- parquet_flask/v1/__init__.py
+++ parquet_flask/v1/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from flask_restx import Api
+from .apidocs import api as apidocs
 from .ingest_json_s3 import api as ingest_parquet_json_s3
 from .replace_json_s3 import api as replace_parquet_json_s3
 from .query_data import api as query_data
@@ -7,7 +8,7 @@ from .query_data_doms import api as query_data_doms
 _version = "1.0"
 
 blueprint = Blueprint('parquet_flask', __name__, url_prefix='/{}'.format(_version))
-
+blueprint.register_blueprint(apidocs)
 
 api = Api(blueprint,
           title='Parquet ingestion & query',
