6db2449324c76025c29b486f78f16213590f8d50
Wai Phyo <wai.phyo@jpl.nasa.gov>
2021-12-07 10:20:31 -0800

    fix: add spark.driver.host to talk to k8s sparl

--- parquet_flask/io_logic/retrieve_spark_session.py
+++ parquet_flask/io_logic/retrieve_spark_session.py
@@ -12,6 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import logging
+from socket import gethostbyname, gethostname
 
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
@@ -19,16 +21,26 @@ from pyspark.sql import SparkSession
 from parquet_flask.utils.config import Config
 from parquet_flask.utils.singleton import Singleton
 
+LOGGER = logging.getLogger(__name__)
+
 
 class RetrieveSparkSession(metaclass=Singleton):
     def __init__(self):
         self.__sparks = {}
+        self.__spark_config = {}
+
+    def set_spark_config(self, input_dict: dict):
+        for k, v in input_dict.items():
+            self.__spark_config[k] = v
+        return self
 
     def retrieve_spark_session(self, app_name, master_spark, ram='1024m') -> SparkSession:
         session_key = '{}__{}'.format(app_name, master_spark)
         if session_key in self.__sparks:
             return self.__sparks[session_key]
         conf = SparkConf()
+        for k, v in self.__spark_config.items():
+            conf.set(k, v)
         """
         spark.executor.memory                   3072m
 spark.hadoop.fs.s3a.impl                org.apache.hadoop.fs.s3a.S3AFileSystem
@@ -41,7 +53,7 @@ spark.driver.extraJavaOptions           -Dcom.amazonaws.services.s3.enableV4=tru
 
 spark.eventLog.enabled              true
 spark.eventLog.dir                  /tmp/spark-events
-spark.eventLog.rolling.enabled      true
+spark.eventLog.rolling.enabled      true    
 spark.eventLog.rolling.maxFileSize  128m
         """
 
@@ -51,7 +63,7 @@ spark.eventLog.rolling.maxFileSize  128m
         # conf.set('spark.eventLog.rolling.maxFileSize', '128m')
         conf.set('spark.executor.memory', ram)  # something
         conf.set('spark.executor.cores', '1')
-        # conf.set('spark.driver.bindAddress', '127.0.0.1')
+        conf.set('spark.driver.port', '50243')
         conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.2.0')  # crosscheck the version.
         conf.set('spark.hadoop.fs.s3a.impl', 'org.apache.hadoop.fs.s3a.S3AFileSystem')
         conf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider')
@@ -60,6 +72,9 @@ spark.eventLog.rolling.maxFileSize  128m
         # conf.set('spark.executor.extraClassPath', '/opt/bitnami/spark/jars/hadoop-aws-3.2.0.jar:/opt/bitnami/spark/jars/aws-java-sdk-bundle-1.11.375.jar')
         # conf.set('spark.executor.extraJavaOptions', '-Dcom.amazonaws.services.s3.enableV4=true')
         # conf.set('spark.driver.extraJavaOptions', '-Dcom.amazonaws.services.s3.enableV4=true')
+        local_ip = gethostbyname(gethostname())
+        LOGGER.debug(f'using IP: {local_ip} for spark.driver.host')
+        conf.set('spark.driver.host', local_ip)
         conf.set('spark.hadoop.fs.s3a.access.key', Config().get_value('aws_access_key_id'))
         conf.set('spark.hadoop.fs.s3a.secret.key', Config().get_value('aws_secret_access_key'))
         conf.set('spark.hadoop.fs.s3a.session.token', Config().get_value('aws_session_token'))
