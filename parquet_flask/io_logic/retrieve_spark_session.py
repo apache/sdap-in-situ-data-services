21b5e8fe2dabbc895d13a660fa7fd1459ece8b2d
Wai Phyo <wai.phyo@jpl.nasa.gov>
2021-12-14 10:35:45 -0800

    fix: allow spark config come from env file

--- parquet_flask/io_logic/retrieve_spark_session.py
+++ parquet_flask/io_logic/retrieve_spark_session.py
@@ -12,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import json
 import logging
 from socket import gethostbyname, gethostname
 
@@ -27,11 +28,35 @@ LOGGER = logging.getLogger(__name__)
 class RetrieveSparkSession(metaclass=Singleton):
     def __init__(self):
         self.__sparks = {}
-        self.__spark_config = {}
+        self.__spark_config = {
+            'spark.executor.cores': '1',  # fixing to 1 core for now
+            'spark.driver.port': '50243',  # a random port.
+            'spark.jars.packages': 'org.apache.hadoop:hadoop-aws:3.2.0',  # crosscheck the version.
+            'spark.hadoop.fs.s3a.impl': 'org.apache.hadoop.fs.s3a.S3AFileSystem',
+            'spark.hadoop.fs.s3a.aws.credentials.provider': 'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider',  # should be overridden
+            'spark.hadoop.fs.s3a.connection.ssl.enabled': 'true',
 
-    def set_spark_config(self, input_dict: dict):
-        for k, v in input_dict.items():
-            self.__spark_config[k] = v
+            # old configs. no longer needs to be used
+            # 'spark.driver.extraJavaOptions': '-Dcom.amazonaws.services.s3.enableV4=true',
+            # 'spark.executor.extraJavaOptions': '-Dcom.amazonaws.services.s3.enableV4=true',
+            # 'spark.executor.extraClassPath': '/opt/bitnami/spark/jars/hadoop-aws-3.2.0.jar:/opt/bitnami/spark/jars/aws-java-sdk-bundle-1.11.375.jar',
+            # 'spark.driver.extraClassPath': '/opt/bitnami/spark/jars/hadoop-aws-3.2.0.jar:/opt/bitnami/spark/jars/aws-java-sdk-bundle-1.11.375.jar',
+            # settings needed for history server. (still an experiment)
+            # 'spark.eventLog.enabled': 'true',
+            # 'spark.eventLog.dir': '/tmp/spark-events',
+            # 'spark.eventLog.rolling.enabled': 'true',
+            # 'spark.eventLog.rolling.maxFileSize': '128m',
+        }
+        self.__set_spark_config()
+
+    def __set_spark_config(self):
+        possible_extra_spark_config = Config().get_value(Config.spark_config_dict, '{}')
+        try:
+            input_dict = json.loads(possible_extra_spark_config)
+            for k, v in input_dict.items():
+                self.__spark_config[k] = v
+        except:
+            LOGGER.exception(f'Not loading extra config. unable to convert to JSON object. {possible_extra_spark_config}.')
         return self
 
     def retrieve_spark_session(self, app_name, master_spark, ram='1024m') -> SparkSession:
@@ -46,38 +71,15 @@ class RetrieveSparkSession(metaclass=Singleton):
 spark.hadoop.fs.s3a.impl                org.apache.hadoop.fs.s3a.S3AFileSystem
 spark.hadoop.fs.s3.impl                 org.apache.hadoop.fs.s3.S3FileSystem
 spark.hadoop.fs.s3n.impl                org.apache.hadoop.fs.s3native.NativeS3FileSystem
-spark.driver.extraClassPath             /usr/bin/spark-3.0.0-bin-hadoop3.2/jars/hadoop-aws-3.2.0.jar:/usr/bin/spark-3.0.0-bin-hadoop3.2/jars/aws-java-sdk-bundle-1.11.563.jar
-spark.executor.extraClassPath           /usr/bin/spark-3.0.0-bin-hadoop3.2/jars/hadoop-aws-3.2.0.jar:/usr/bin/spark-3.0.0-bin-hadoop3.2/jars/aws-java-sdk-bundle-1.11.563.jar
-spark.executor.extraJavaOptions         -Dcom.amazonaws.services.s3.enableV4=true
-spark.driver.extraJavaOptions           -Dcom.amazonaws.services.s3.enableV4=true
-
-spark.eventLog.enabled              true
-spark.eventLog.dir                  /tmp/spark-events
-spark.eventLog.rolling.enabled      true    
-spark.eventLog.rolling.maxFileSize  128m
         """
 
-        # conf.set('spark.eventLog.enabled', 'true')
-        # conf.set('spark.eventLog.dir', '/tmp/spark-events')
-        # conf.set('spark.eventLog.rolling.enabled', 'true')
-        # conf.set('spark.eventLog.rolling.maxFileSize', '128m')
         conf.set('spark.executor.memory', ram)  # something
-        conf.set('spark.executor.cores', '1')
-        conf.set('spark.driver.port', '50243')
-        conf.set('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:3.2.0')  # crosscheck the version.
-        conf.set('spark.hadoop.fs.s3a.impl', 'org.apache.hadoop.fs.s3a.S3AFileSystem')
-        conf.set('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider')
-        conf.set('spark.hadoop.fs.s3a.connection.ssl.enabled', 'true')
-        # conf.set('spark.driver.extraClassPath', '/opt/bitnami/spark/jars/hadoop-aws-3.2.0.jar:/opt/bitnami/spark/jars/aws-java-sdk-bundle-1.11.375.jar')
-        # conf.set('spark.executor.extraClassPath', '/opt/bitnami/spark/jars/hadoop-aws-3.2.0.jar:/opt/bitnami/spark/jars/aws-java-sdk-bundle-1.11.375.jar')
-        # conf.set('spark.executor.extraJavaOptions', '-Dcom.amazonaws.services.s3.enableV4=true')
-        # conf.set('spark.driver.extraJavaOptions', '-Dcom.amazonaws.services.s3.enableV4=true')
         local_ip = gethostbyname(gethostname())
         LOGGER.debug(f'using IP: {local_ip} for spark.driver.host')
         conf.set('spark.driver.host', local_ip)
-        conf.set('spark.hadoop.fs.s3a.access.key', Config().get_value('aws_access_key_id'))
-        conf.set('spark.hadoop.fs.s3a.secret.key', Config().get_value('aws_secret_access_key'))
-        conf.set('spark.hadoop.fs.s3a.session.token', Config().get_value('aws_session_token'))
+        conf.set('spark.hadoop.fs.s3a.access.key', Config().get_value(Config.aws_access_key_id))
+        conf.set('spark.hadoop.fs.s3a.secret.key', Config().get_value(Config.aws_secret_access_key))
+        conf.set('spark.hadoop.fs.s3a.session.token', Config().get_value(Config.aws_session_token))
         # conf.set('spark.default.parallelism', '10')
         # conf.set('spark.hadoop.fs.s3a.endpoint', 's3.us-gov-west-1.amazonaws.com')
         self.__sparks[session_key] = SparkSession.builder.appName(app_name).config(conf=conf).master(master_spark).getOrCreate()
