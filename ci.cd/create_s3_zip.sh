576b1c18806e474d5a7c388bf1cdd88e612e73be
Wai Phyo <wai.phyo@jpl.nasa.gov>
2022-07-22 15:04:43 -0700

    fix: get lambda function working

--- ci.cd/create_s3_zip.sh
+++ ci.cd/create_s3_zip.sh
@@ -7,13 +7,13 @@ project_root_dir=${PWD}
 software_version=`python3 ${project_root_dir}/setup.py --version`
 zip_file="${project_root_dir}/${ZIP_NAME}__${software_version}.zip" ; # save the result file in current working directory
 
-tmp_proj='/tmp/cdms_lambda_functions'
+tmp_proj='/tmp/parquet_flask'
 
 source_dir="/usr/local/lib/python3.8/site-packages/"
 
-mkdir -p "$tmp_proj/cdms_lambda_functions" && \
+mkdir -p "$tmp_proj/parquet_flask" && \
 cd $tmp_proj && \
-cp -a "${project_root_dir}/parquet_flask/." "$tmp_proj/cdms_lambda_functions" && \
+cp -a "${project_root_dir}/parquet_flask/." "$tmp_proj/parquet_flask" && \
 cp "${project_root_dir}/setup_lambda.py" $tmp_proj && \
 python3 setup_lambda.py install && \
 python3 setup_lambda.py install_lib && \
--- parquet_flask/cdms_lambda_func/index_to_es/execute_lambda.py
+++ parquet_flask/cdms_lambda_func/index_to_es/execute_lambda.py
@@ -1,6 +1,15 @@
-from parquet_flask.cdms_lambda_func.index_to_es.parquet_file_es_indexer import ParquetFileEsIndexer
+import os
+
 
 
 def execute_code(event, context):
+    os.environ['master_spark_url'] = ''
+    os.environ['spark_app_name'] = ''
+    os.environ['parquet_file_name'] = ''
+    os.environ['in_situ_schema'] = ''
+    os.environ['authentication_type'] = ''
+    os.environ['authentication_key'] = ''
+    os.environ['parquet_metadata_tbl'] = ''
+    from parquet_flask.cdms_lambda_func.index_to_es.parquet_file_es_indexer import ParquetFileEsIndexer
     ParquetFileEsIndexer().start(event)
     return
