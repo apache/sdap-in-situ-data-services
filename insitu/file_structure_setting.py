4c55fb2fb7f2f5e5da3047269d856127d297c768
Wai Phyo <wai.phyo@jpl.nasa.gov>
2023-02-22 13:52:46 -0800

    chore: add missing file

---
+++ insitu/file_structure_setting.py
@@ -0,0 +1,25 @@
+STRUCTURE_CONFIG = {
+    "type": "object",
+    "required": ["partitioning_columns", "non_data_columns", "derived_columns"],
+    "partitioning_columns": {"type": "array", "items": {"type": "string"}},
+    "non_data_columns": {"type": "array", "items": {"type": "string"}},
+    "derived_columns": {
+        "type": "object",
+        "required": [],
+        "properties": {}
+    }
+
+}
+
+
+class FileStructureSetting:
+    def __init__(self, data_json_schema: dict, structure_config: dict):
+        self.__data_json_schema = data_json_schema
+        self.__structure_config = structure_config
+
+    def get_derived_columns(self):
+        return self.__structure_config['derived_columns']
+
+    def get_partitioning_columns(self):
+        return self.__structure_config['partitioning_columns']
+
