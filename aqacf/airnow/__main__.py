0a6bc6527df4aa9f2650f84de0db07a0961cfa23
Wai Phyo <wai.phyo@jpl.nasa.gov>
2023-10-02 10:24:22 -0700

    fix: hide default ENV + sample docker-compose file

--- aqacf/airnow/__main__.py
+++ aqacf/airnow/__main__.py
@@ -4,11 +4,11 @@ import os
 from aqacf.airnow.airnow_wrapper import AirNowWrapper
 
 
-os.environ['LOG_LEVEL'] = '10'
-os.environ['provider'] = 'AirNow'
-os.environ['project'] = 'air_quality'
-os.environ['staging_bucket'] = 'aq-in-situ-data-staging'
-os.environ['year'] = '2017'
+# os.environ['LOG_LEVEL'] = '10'
+# os.environ['provider'] = 'AirNow'
+# os.environ['project'] = 'air_quality'
+# os.environ['staging_bucket'] = 'aq-in-situ-data-staging'
+# os.environ['year'] = '2017'
 if __name__ == '__main__':
     logging.basicConfig(level=int(os.getenv('LOG_LEVEL', '20')),
                         format="%(asctime)s [%(levelname)s] [%(name)s::%(lineno)d] %(message)s")
@@ -18,3 +18,5 @@ if __name__ == '__main__':
     AirNowWrapper(os.environ.get('provider'),
                   os.environ.get('project'),
                   os.environ.get('staging_bucket')).start(int(os.environ.get('year')))
+# CI/CD is successful.
+# https://hub.docker.com/layers/waiphyojpl/cdms.parquet.flask/build-aqacf-3/images/sha256-754d57d785e8a8723e95eaf98aec7c9dee88a25f1899135a0344cf1e2fc04f6c?context=repo
