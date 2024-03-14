25fbbd0127877423bb5b15509dec6c3dab36fc93
Wai Phyo <wai.phyo@jpl.nasa.gov>
2024-03-14 13:02:20 -0700

    fix: secret scan updates

--- parquet_flask/cdms_lambda_func/s3_records/s3_2_sqs.py
+++ parquet_flask/cdms_lambda_func/s3_records/s3_2_sqs.py
@@ -18,18 +18,18 @@
   "Records": [
     {
       "messageId": "6210f778-d081-4ae9-a861-8534d612dfae",
-      "receiptHandle": "AQEBk55DchogyQzpVsH1A4YEj4K/PcVuIG9Em/a6/4AHIA4G5vLPiHVElNiuMfYc1ussk2U//JwZbD788Fv8u6W22L3AJ1U8EIcGJ57aibpmd6tSCWLS5q5FA4u2X2Jq5z+lCX5NZXzNDYMqMJaCGtBkcYi4a9LDXtD+U7HWX0V8OPhFFF2a1qUu+E05c16f5OmE7wRJ3SFrRmtJOhp2DigKKsw6VJtZklTm6uILMOL1ETOTlbA02dhF16fjcXlAACirDp0Yo9pi91FrpEljOYkqAO9AX4WMbEjAPZrnaATfYmRqCTOlnrIK8xvgEPgIu/OOub7KBYh6AQn7U8QBNoASkXkn31dqyM2I+KosKy2VeJO9cjPTahhXtkW7zUFA6863Czt2oHqL6Rvwsjr+7TikfQ==",
-      "body": "{\"Records\":[{\"eventVersion\":\"2.1\",\"eventSource\":\"aws:s3\",\"awsRegion\":\"us-gov-west-1\",\"eventTime\":\"2022-02-07T17:31:04.498Z\",\"eventName\":\"ObjectCreated:Put\",\"userIdentity\":{\"principalId\":\"AWS:AROAWM7XM4I6Z3NL2ST2J:wphyo\"},\"requestParameters\":{\"sourceIPAddress\":\"128.149.246.219\"},\"responseElements\":{\"x-amz-request-id\":\"FM1CHA780PBP0YEY\",\"x-amz-id-2\":\"Vp12Q/ok1+Y/0WonTCoUjCCREZhJU3CO82uDbve6m6FqJsFGTMBcLdunqeMLmQ11ZECV6z2WFsak6EbjdIZTi/jL+crmwops\"},\"s3\":{\"s3SchemaVersion\":\"1.0\",\"configurationId\":\"all-obj-create\",\"bucket\":{\"name\":\"lsmd-data-bucket\",\"ownerIdentity\":{\"principalId\":\"440216117821\"},\"arn\":\"arn:aws-us-gov:s3:::lsmd-data-bucket\"},\"object\":{\"key\":\"manual_test/zipped_upload/jpl.calendar.2022.png\",\"size\":841141,\"eTag\":\"1477b70ad2cd03be3d72a49dc58fb52a\",\"sequencer\":\"0062015756ACFAA1FD\"}}}]}",
+      "receiptHandle": "<encoded id>",
+      "body": "<JSON String. See Below>",
       "attributes": {
         "ApproximateReceiveCount": "6",
         "SentTimestamp": "1644255065441",
-        "SenderId": "AIDALVP5ID7KAVBU2CQ3O",
+        "SenderId": "<ID>",
         "ApproximateFirstReceiveTimestamp": "1644255065441"
       },
       "messageAttributes": {},
-      "md5OfBody": "00cb0a5ed122862537ab6115dae36f69",
+      "md5OfBody": "<MD5>",
       "eventSource": "aws:sqs",
-      "eventSourceARN": "arn:aws-us-gov:sqs:us-gov-west-1:440216117821:send_records_to_es",
+      "eventSourceARN": "arn:aws-us-gov:sqs:<REGION>:<ACCOUNT-ID>:send_records_to_es",
       "awsRegion": "us-gov-west-1"
     }
   ]
@@ -44,14 +44,14 @@
       "eventTime": "2022-02-07T17:31:04.498Z",
       "eventName": "ObjectCreated:Put",
       "userIdentity": {
-        "principalId": "AWS:AROAWM7XM4I6Z3NL2ST2J:wphyo"
+        "principalId": "AWS:ID:USER"
       },
       "requestParameters": {
         "sourceIPAddress": "128.149.246.219"
       },
       "responseElements": {
-        "x-amz-request-id": "FM1CHA780PBP0YEY",
-        "x-amz-id-2": "Vp12Q/ok1+Y/0WonTCoUjCCREZhJU3CO82uDbve6m6FqJsFGTMBcLdunqeMLmQ11ZECV6z2WFsak6EbjdIZTi/jL+crmwops"
+        "x-amz-request-id": "ID",
+        "x-amz-id-2": "ID"
       },
       "s3": {
         "s3SchemaVersion": "1.0",
@@ -66,8 +66,8 @@
         "object": {
           "key": "manual_test/zipped_upload/jpl.calendar.2022.png",
           "size": 841141,
-          "eTag": "1477b70ad2cd03be3d72a49dc58fb52a",
-          "sequencer": "0062015756ACFAA1FD"
+          "eTag": "tag",
+          "sequencer": "ID"
         }
       }
     }
