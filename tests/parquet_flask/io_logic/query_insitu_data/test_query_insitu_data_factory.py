afa1cd9973f4489fa0ee45d415ff619de0240053
Wai Phyo <wai.phyo@jpl.nasa.gov>
2024-04-17 10:56:28 -0700

    fix: update insitu test case

--- tests/parquet_flask/io_logic/query_insitu_data/test_query_insitu_data_factory.py
+++ tests/parquet_flask/io_logic/query_insitu_data/test_query_insitu_data_factory.py
@@ -7,11 +7,12 @@ from parquet_flask.io_logic.query_v2 import QueryProps
 class TestQueryInsituDataFactory(TestCase):
     def test_gmu(self):
         query_props = QueryProps()
-        query_props.provider = 'PurpleAir-GMU-Intermediate'
+        # query_props.provider = 'PurpleAir-GMU-Intermediate'
+        query_props.provider = 'PurpleAir-GMU-Raw-Hourly'
         query_props.project = 'air_quality'
         query_props.min_datetime = '2023-01-01T00:00:00'
-        query_props.max_datetime = '2023-01-07T00:00:00'
-        query_props.platform_id = ['195']
+        query_props.max_datetime = '2023-01-07T23:00:00'
+        query_props.platform_id = ['9678']
         stats = QueryInsituDataFactory().get_instance(query_props.provider, query_props=query_props, base_url='https://insitu-api.stcenter.net')
         results = stats.search()
         print(results)
